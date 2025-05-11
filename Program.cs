f52d2eaefb32f34f55defc8c61a6253dbdb0ec92
hikari <you@example.com>
2025-05-11 12:08:46 +0900

    HuTaoクライアントとタイマーをstaticで初期化するよう変更

--- Program.cs
+++ Program.cs
@@ -4,34 +4,52 @@ using HuTao.NET;
 
 class Program
 {
-    private static DiscordSocketClient _client = new DiscordSocketClient();
+    // Discordとやりとりするためのクライアント
+    private static DiscordSocketClient _discordClient = new DiscordSocketClient();
+
+    // hoyolabからゲーム内情報を取得するクライアント
+    // CookieV2は、ブラウザでhoyolabにログインし、開発者モードを開いて設定値を取得する
+    private static Client _hutaoClient = new Client(
+        new CookieV2()
+        {
+            // ltmid_v2 を設定
+            ltmid_v2 = "",
+            // ltoken_v2 を設定
+            ltoken_v2 = "",
+            // ltuid_v2 を設定
+            ltuid_v2 = ""
+        },
+        new ClientData() { Language = "ja-jp" }
+    );
 
     // botのトークン
-    private static string Token = ""; 
+    private static string Token = "";
     // 通知するチャンネルのID
-    private static ulong ChannelId = 0; 
+    private static ulong ChannelId = 0;
     // 原神のユーザーID
     private static int GenshinUid = 0;
 
+    // 1時間ごとのタイマーを設定する
+    private static System.Timers.Timer _timer = new System.Timers.Timer(TimeSpan.FromHours(1).TotalMilliseconds);
+
     static async Task Main()
     {
-        _client.Log += Log;
+        _discordClient.Log += Log;
 
-        await _client.LoginAsync(TokenType.Bot, Token);
-        await _client.StartAsync();
+        await _discordClient.LoginAsync(TokenType.Bot, Token);
+        await _discordClient.StartAsync();
 
         // Botが準備完了したら、樹脂チェックイベントを実行する
-        _client.Ready += CheckResinEvent;
+        _discordClient.Ready += CheckResinEvent;
 
         await Task.Delay(Timeout.Infinite);
     }
 
     private static async Task CheckResinEvent()
     {
-        //  1時間ごとに樹脂の状態を確認
-        var timer = new System.Timers.Timer(TimeSpan.FromHours(1).TotalMilliseconds);
-        timer.Elapsed += (sender, e) => Task.Run(async () => await CheckResinAsync());
-        timer.Start();
+        // 一定時間おきに樹脂の状態を確認する
+        _timer.Elapsed += async (sender, e) => await CheckResinAsync();
+        _timer.Start();
 
         await Task.Delay(Timeout.Infinite);
     }
@@ -40,23 +58,10 @@ class Program
     {
         const int MaxResin = 200;
 
-        // ブラウザでhoyolabにログインし、開発者モードを開いて取得する
-        CookieV2 cookieV2 = new CookieV2()
-        {
-            // ltmid_v2 を設定
-            ltmid_v2 = "",
-            // ltoken_v2 を設定
-            ltoken_v2 = "", 
-            // ltuid_v2 を設定
-            ltuid_v2 = ""     
-        };
-
-        Client client = new Client(cookieV2, new ClientData() { Language = "ja-jp" });
-
         try
         {
             // リアルタイムで更新されるデータを取得してくる
-            var res = await client.FetchDailyNote(new GenshinUser(GenshinUid));
+            var res = await _hutaoClient.FetchDailyNote(new GenshinUser(GenshinUid));
 
             if (res.data == null)
             {
@@ -71,10 +76,13 @@ class Program
                 string message = $"おい旅人！あと {FormatTimeSpan(res.data.ResinRecoveryTime)} で樹脂が溢れるぞ！";
                 await SendMessageToDiscord(message);
             }
+
+            string message2 = $"おい旅人！あと {FormatTimeSpan(res.data.ResinRecoveryTime)} で樹脂が溢れるぞ！";
+                await SendMessageToDiscord(message2);
         }
         catch (Exception ex)
         {
-            string message = $"おい旅人！予想外のエラーが発生したぞ！ {ex}";
+            string message = $"おい旅人！想定外のエラーが発生したぞ！ {ex}";
             await SendMessageToDiscord(message);
         }
     }
@@ -82,7 +90,7 @@ class Program
     // Discordチャンネルにメッセージを送信
     private static async Task SendMessageToDiscord(string message)
     {
-        var channel = _client.GetChannel(ChannelId) as IMessageChannel;
+        var channel = _discordClient.GetChannel(ChannelId) as IMessageChannel;
         if (channel != null)
         {
             await channel.SendMessageAsync(message);
